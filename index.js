9eb29e685c7a2f396ca2cbfdac06c754c08749c2
Sultan Mahmud <smrsaimon960@gmail.com>
2021-04-02 02:14:32 +0600

    hh

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const app = require('./app')
 
 const port = process.env.PORT || 5000;
-// host: process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS
+
 app.listen(port, () => {
 
   console.log(`Server running on port ${port} `)
